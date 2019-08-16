6ab0d09c47c18bd1dfc365a1d4116a87d8ade748
abelockii80 <abelockii80@mail.ru>
2019-08-16 16:16:34 +0300

    Перехожу на другой комп

---
+++ tmp.py
@@ -0,0 +1,16 @@
+import sqlite3
+
+
+
+def list_from_base_column(column): # Возвращает список значений столбца
+    conn = sqlite3.connect('users.db')
+    cursor = conn.cursor()
+    cursor.execute(f'SELECT {column} FROM users WHERE role=admin')
+    column_list = cursor.fetchall()
+    conn.commit()
+    conn.close()
+    return column_list
+
+column = ('chat_id')
+p = list_from_base_column(column)
+print(p)
