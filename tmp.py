a96b502ec84ac07a603dfde202eb94a35832f2f4
abelockii80 <abelockii80@mail.ru>
2019-08-19 21:16:31 +0300

    Не работает выборка строк из базы. причина не понятна

--- tmp.py
+++ tmp.py
@@ -1,3 +1,29 @@
-text = 'На бойлере, как положено, пришлепан лейбл компании, обслуживающей это чудо техники. \n \nПозвонил и вызвал мастера.'
-new_text = text.replace(' \n \n', '\n\n')
-print(new_text)
+import sqlite3
+import random
+import logging
+
+logging.basicConfig(format='%(asctime)s - %(levelname)s - %(message)s',
+                    level = logging.INFO,
+                    filename = 'bot.log'
+   
+
+  )
+
+
+
+
+def execute_data_from_base(tablename): 
+    conn = sqlite3.connect('list_of_posts_base.db')
+    cursor = conn.cursor()
+    cursor.execute(f"SELECT count(*) FROM {tablename}")
+    count_items = cursor.fetchone()[0]    
+    rand_numb = random.randint(1, int(count_items))
+    cursor.execute("SELECT post_text, img, post_date, post_to, id FROM {} where rowid = ?".format(tablename), (rand_numb,))
+    logging.info(f'Выборка из базы, cursor.fetchall(): {cursor.fetchall()}')
+    data = cursor.fetchall()
+        
+    conn.close()      
+    return data  
+
+data = execute_data_from_base('story_holodkova')     
+print(data)
