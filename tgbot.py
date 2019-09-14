1289e10636d712f00a5fc3467b7fc33748ef998c
abelockii80 <abelockii80@mail.ru>
2019-09-14 08:05:13 +0300

    Работает веб-хук. Нужно в урл добавить токен, чтобы никто не знал веб-хук урл

--- tgbot.py
+++ tgbot.py
@@ -1,11 +1,16 @@
 import datetime
+import json
 import logging
 import random
 import sqlite3
 
-from telegram.ext import Updater, CallbackQueryHandler, ConversationHandler, CommandHandler, \
+from flask import Flask
+from flask import request, redirect
+from telegram import Update, Bot
+from telegram.ext import Dispatcher, JobQueue, Updater, CallbackQueryHandler, ConversationHandler, CommandHandler, \
         MessageHandler, RegexHandler, Filters
 from telegram.ext import messagequeue as mq
+from queue import Queue
 
 import settings
 import handlers
@@ -18,8 +23,20 @@ logging.basicConfig(format='%(asctime)s - %(levelname)s - %(funcName)s - %(messa
                     filename = 'bot.log'
                     )
 
+logger = logging.getLogger(__name__)
 
-# TODO Изменить клавиатуру, которая приходит юзерам
+
+bot = Bot(settings.TOKEN)
+update_queue = Queue()
+
+dp = Dispatcher(bot, update_queue)
+j = JobQueue(bot)
+
+app = Flask(__name__)
+
+
+
+
 # TODO Когда админ удаляет сообщение, чтобы удалялась картинка
 
 
@@ -27,20 +44,20 @@ logging.basicConfig(format='%(asctime)s - %(levelname)s - %(funcName)s - %(messa
 
 
 def main():
-    mybot = Updater(settings.TOKEN)
+
 
     # Инициализируем MessageQueue 
-    mybot.bot._msg_queue = mq.MessageQueue()
-    mybot.bot._is_messages_queued_default=True
+    dp.bot._msg_queue = mq.MessageQueue()
+    dp.bot._is_messages_queued_default=True
 
 
     logging.info('Бот запускается.')
 
-    dp = mybot.dispatcher
+
 
     # mybot.job_queue.run_repeating(handlers.send_updates, interval=utils.set_interval(settings.POST_COUNT), first=300)
-    mybot.job_queue.run_repeating(handlers.send_updates, interval=300, first=300)
-    mybot.job_queue.run_repeating(handlers.admin_handle_posts_to_tg, interval=1800, first=1)
+    j.run_repeating(handlers.send_updates, interval=300, first=300)
+    j.run_repeating(handlers.admin_handle_posts_to_tg, interval=1800, first=1)
     # mybot.job_queue.run_daily(handlers.admin_handle_posts_to_tg, time=settings.ADMIN_TIME)
     
     admin_mode = ConversationHandler(
@@ -56,22 +73,29 @@ def main():
     dp.add_handler(CallbackQueryHandler(handlers.func))
     dp.add_handler(admin_mode)   
     dp.add_handler(CommandHandler('start', handlers.subscribe))
-    dp.add_handler(CommandHandler('unsubscribe', handlers.unsubscribe))  
+    dp.add_handler(CommandHandler('unsubscribe', handlers.unsubscribe))     
     
-    # dp.add_handler(MessageHandler(Filters.text, test_message))
-    
-    # webhook_domain = 'https://python-developer.ru'
-    # webhook_domain = 'https://9dc689bc.ngrok.io'
-    # PORT = 88
-    
-    # mybot.start_webhook(listen='0.0.0.0',
-    #                 port=PORT,
-    #                 url_path=settings.TOKEN,
-    #                 webhook_url=f'{webhook_domain}:{str(PORT)}/{settings.TOKEN}')
-    
-    mybot.start_polling(poll_interval=5.0)
-    mybot.idle()
+
+
+main()
+
+@app.route('/', methods=['POST', 'GET'])
+def webhook():
+    if request.method == "POST":
+        # retrieve the message in JSON and then transform it to Telegram object
+        update = Update.de_json(request.get_json(force=True), bot)
+
+        logger.info("Update received! " + update.message.text)
+        dp.process_update(update)
+        update_queue.put(update)
+        return "OK"
+    else:
+        return redirect("https://telegram.me/links_forward_bot", code=302)
+
+
+
 
 
 if __name__ == '__main__':
-    main()
+    app.run(host='0.0.0.0',
+            debug=True)
